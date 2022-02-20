d5382e13168db3f5b603b2f49dbdb607f4cd3013
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2022-02-20 16:47:46 +0800

    Update aget.test.py

--- aget/src/aget.test.py
+++ aget/src/aget.test.py
@@ -4,6 +4,7 @@ from modules import Config
 from modules import (param, fill)
 
 config = Config(Config.rconf("./config.ci.json"))
+url = param.parse(config)[0]
 parameters = param.parse(config)[1]
 
 dat = json.loads(parameters)
